09b27ffab5ed1918609f55d5b34e3ce31c6b7a76
scientist-Momi <scientistmomi22@gmail.com>
2025-08-19 13:49:27 +0100

    Working on product return

--- src/components/modals/ReturnModal.vue
+++ src/components/modals/ReturnModal.vue
@@ -1,9 +1,19 @@
 <script setup>
+import { ref, onMounted } from 'vue'
+import PrimaryButton from '../buttons/PrimaryButton.vue'
+import SecondaryButton from '../buttons/SecondaryButton.vue'
+import PageLoader from '../PageLoader.vue';
+import { useModalStore } from '@/stores/modalStore'
+import { useToastStore } from '@/stores/toastStore'
+
+const toast = useToastStore()
 </script>
 
 <template>
-  <div>
-    <h1>ReturnModal.vue</h1>
+  <div class="bg-white h-full">
+    <div v-if="loading">
+      <PageLoader />
+    </div>
   </div>
 </template>
 
