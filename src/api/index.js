b42d85a6752c51cb8c596611910dc744350f2c94
scientist-Momi <scientistmomi22@gmail.com>
2025-08-31 03:56:16 +0100

    Making few changes

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,8 @@
 import axios from 'axios';
+import router from '@/router';
+import { useAuthStore } from '@/stores/authStore';
+
+const auth = useAuthStore()
 
 const instance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/v1',
@@ -10,6 +14,18 @@ instance.interceptors.request.use((config) => {
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
+
+instance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      auth.logout()
+      router.push('/login')
+      return Promise.reject(error)
+    }
+    return Promise.reject(error)
+  },
+)
 
 export default instance;
 
